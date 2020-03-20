05956dd0f3d258a3335a6b58b9387be84bb30ee6
Cristiano <valentisenior@gmail.com>
2020-03-20 17:51:39 +0100

    primo e secondo

---
+++ main.js
@@ -0,0 +1,22 @@
+// L'utente inserisce due nuemri in successione. Il software stampa il maggiore
+
+// messaggio da dare all'utente
+var msg;
+// L'utente inserisce il primo numero e lo salviamo
+var primoNumeroUtente = parseInt(prompt("Ciao , inserisci per favore un numero"));
+// L'utente inserisce il secondo nuemro e lo salviao
+var secondoNuemroUtente = parseInt(prompt("Inserisci ora un secondo nuemro "));
+
+console.log(primoNumeroUtente);
+console.log(secondoNuemroUtente);
+
+// Confronto i due numeri per vedere qual è quella più lunga
+if (primoNumeroUtente > secondoNuemroUtente){
+  msg = "Il primo numero che hai scritto è più grande";
+} else if (secondoNuemroUtente > primoNumeroUtente){
+  msg = "Il secondo numero è più grande";
+} else{
+  msg = "i due numero sono uguali"
+}
+
+document.getElementById("numeri").innerHTML = msg;
---
+++ secondjs.js
@@ -0,0 +1,21 @@
+// L’utente inserisce due parole in successione, con due prompt. Il software stampa prima la parola più corta, poi la parola più lunga.
+
+var msg ;
+// Chiedere all'utente di inserire la prima parola
+var primaParolaUtente = prompt("Ciao , se non hai altro da fare inserisci una paola , per favore");
+// Chiedere all'utente di inserire una seconda parola
+var secondaParolaUtente = prompt("Per favore visto che sei a casa a nonfare niente , inserisci un'altra parola");
+
+
+console.log(primaParolaUtente.length);
+console.log(secondaParolaUtente.length);
+// Stampare output con parola più corta prima e parola più lunga dopo
+
+if (primaParolaUtente.length > secondaParolaUtente.length){
+  msg = secondaParolaUtente + primaParolaUtente;
+} else if (secondaParolaUtente.length > primaParolaUtente.length){
+  msg = primaParolaUtente + secondaParolaUtente;
+}
+
+
+document.getElementById("paroleacaso").innerHTML = msg;
